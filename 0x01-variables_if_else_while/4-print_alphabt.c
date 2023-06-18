4be01ea851b7a9796061b41c3d8e3025fd6273e4
El Mehdi Amaatouch <elmehdiamaatouch@gmail.com>
2023-06-18 10:36:41 -0700

    correction

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -5,13 +5,16 @@
  * Description:  Print all the letters except q and e
  * Return: 0
  */
-int main()
+int main(void)
 {
-    char c = 'a';
-    while ( c<='z'){
-        if (c != 'e' && c != 'q' )
-        putchar(c);
-        c++;}
-        putchar('\n');
-return (0);
+		char c;
+
+		for (c = 'a'; c <= 'z'; c++)
+		{
+		if (c != 'e' && c != 'q')
+		putchar(c);
+		}
+		putchar('\n');
+
+	return (0);
 }
