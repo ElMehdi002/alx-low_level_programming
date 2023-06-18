ae8deed60beb7f78afc24f5b912165216b68be8b
El Mehdi Amaatouch <elmehdiamaatouch@gmail.com>
2023-06-18 10:24:57 -0700

    correction

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+ * main - main block
+ * Description:  Print all the letters except q and e
+ * Return: 0
+ */
 int main()
 {
     char c = 'a';
     while ( c<='z'){
-        if (c != 'e' && c != 'q' ){
-            putchar(c);
-        }
-        c++;
-        }
-    putchar('\n');
+	    if (c != 'e' && c != 'q' )}
+    putchar(c);
+}
+c++;
+}
+putchar('\n');
 
     return 0;
 }
