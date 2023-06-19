1f373389b841c128ea6aa0ceb05e75258c3e8fe6
El Mehdi Amaatouch <elmehdiamaatouch@gmail.com>
2023-06-18 17:51:07 -0700

    100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,23 +7,23 @@
  */
 int main(void)
 {
-	int i1;
-	int i2;
+    int i1;
+    int i2;
 
-	for (i1 = 0; i1 <= 9; i1++)
-	{
-		for (i2 = i1 + 1; i2 <= 9; i2++)
-		{
-			putchar(i1 + '0');
-			putchar(i2 + '0');
+    for (i1 = 0; i1 <= 9; i1++)
+    {
+        for (i2 = i1 + 1; i2 <= 9; i2++)
+        {
+            putchar(i1 + '0');
+            putchar(i2 + '0');
 
-			if (i1 != 8 || i2 != 9)
-				putchar(',');
-				putchar(' ');
-		}
-	}
+            if (i1 != 9 || i2 != 8)
+                putchar(' ');
+        }
+    }
 
-	putchar('\n');
+    putchar('\n');
 
-	return (0);
+    return 0;
 }
+
