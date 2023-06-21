aa3869dfddea8edbb6dc6863fe9476295370ed60
El Mehdi Amaatouch <elmehdiamaatouch@gmail.com>
2023-06-21 11:07:42 -0700

    alx

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+include "main.h"
+/**
+* print_alphabet - Prints the alphabet in lowercase.
+*
+* Return: Always 0 (success)
+*/
+int main()
+{char c = 'a':
+	while(c <= 'z'){
+		_putchar(c);
+		c++;
+	}
+return(0);
+}
