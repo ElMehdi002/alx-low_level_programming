05917f0e4142c28b1b950a8e3d4a940964362975
El Mehdi Amaatouch <elmehdiamaatouch@gmail.com>
2023-06-21 13:02:40 -0700

    10alpha

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,21 +1,19 @@
 #include "main.h"
-
 /**
- * print_alphabet_x10 - Prints the alphabet 10 times.
- *
- * Return: void
- */
+* print_alphabet_x10 - Prints the alphabet 10 times.
+*
+* Return: Always 0 (success)
+*/
 void print_alphabet_x10(void)
 {
-        int c;
-        int i;
-
-        for (i = 0; i < 10; i++)
-        {
-                for (c = 97; c <= 122; c++)
-                {
-                        _putchar(c);
-                }
-                _putchar('\n');
-        }
+	int i;
+	int c;
+	for (i = 0; i < 10; i++)
+	{
+		fot (c = 97; c = 122; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
 }
