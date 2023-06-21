45ea6c30154524d903b498943e58ae16c3da46b6
El Mehdi Amaatouch <elmehdiamaatouch@gmail.com>
2023-06-21 12:38:47 -0700

    10alpha

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,15 +5,14 @@
 * Return: Always 0 (success)
 */
 void print_alphabet_x10(void)
-{int c, i;
+{
+	int c, i;
 	for (i = 0; i < 10; i++)
 	{
-        int c;
-
-        for (c = 97; c <= 122; c++)
-        {
-                _putchar(c);
-        }
-        _putchar('\n');
+        	for (c = 97; c <= 122; c++)
+        	{
+                	_putchar(c);
+        	}
+        	_putchar('\n');
 	}
 }
