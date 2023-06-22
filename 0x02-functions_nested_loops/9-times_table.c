1f8165f1db6e006d91657e33b36bc557537d606a
El Mehdi Amaatouch <elmehdiamaatouch@gmail.com>
2023-06-21 17:40:57 -0700

    9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,4 +1,7 @@
 #include "main.h"
+
+int _putchar(char c);
+
 /**
  * times_table - Prints the 9 times table.
  *
@@ -15,15 +18,25 @@ void times_table(void)
 			result = i * j;
 
 			if (j != 0)
-				printf(", ");
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 
 			if (result <= 9)
-				printf(" ");
-			
-			printf("%d", result);
+				_putchar(' ');
+
+			if (result >= 10)
+			{
+				_putchar((result / 10) + '0');
+				_putchar((result % 10) + '0');
+			}
+			else
+			{
+				_putchar(result + '0');
+			}
 		}
 
-		printf("\n");
+		_putchar('\n');
 	}
 }
-
