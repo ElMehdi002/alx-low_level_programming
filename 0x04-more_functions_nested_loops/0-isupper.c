586055806a367903d256e14cbb25563219d0a16e
El Mehdi Amaatouch <elmehdiamaatouch@gmail.com>
2023-06-22 20:16:48 -0700

    more

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isupper - Check if paramter c represents an uppercase letter.
+ * @c: Int representing a character
+ * Return: Always 0.
+ */
+int _isupper(int c)
+{
+	int c;
+
+	if (c >= 41 && c <= 90)
+		return (1);
+	else
+		return (0);
