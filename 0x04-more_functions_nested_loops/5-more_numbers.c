945ca78e2412f3707f6f55fd18e5c82e0d006c37
El Mehdi Amaatouch <elmehdiamaatouch@gmail.com>
2023-07-05 09:32:11 -0700

    0x08-recursion

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * more_numbers - Print numbers between 0 to 14 incl.
+ *
+ * Return: Void.
+ */
+void more_numbers(void)
+{
+	for(i = 0; i < 15; i++)
