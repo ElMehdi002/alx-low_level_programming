aa396f507cbc5ae34e6adce4632f1465602e499e
El Mehdi Amaatouch <elmehdiamaatouch@gmail.com>
2023-07-05 15:01:26 -0700

    5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,6 +3,7 @@
 /**
  * _sqrt_recursion - Find the natural square root of a number.
  * @n: The number to find the square root of.
+ * @val: The current value being checked as a possible square root.
  * Return: The square root as an integer.
  */
 
@@ -16,6 +17,13 @@ int _sqrt_recursion(int n)
  * square - Helper function to find the square root recursively.
  * @n: The number to find the square root of.
  * @val: The current value being checked as a possible square root.
+ * Description: This function takes a number 'n' and a value 'val'.
+ *              It recursively checks if 'val' * 'val' is equal to 'n'.
+ *              If it is, 'val' is the square root of 'n' and returned.
+ *              If 'val' * 'val' is less than 'n', the function is called
+ *              recursively with an incremented value. If 'val' * 'val'
+ *              becomes greater than 'n', -1 is returned to indicate that
+ *              the square root does not exist as an integer.
  * Return: The square root as an integer if found, or -1 if not.
  */
 
